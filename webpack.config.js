814bf1965ff78d047cb528aeb1fb258f45403af7
Artem Lipodat <a.lipodat@mail.ru>
2023-01-11 15:31:04 +0400

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const autoprefixer = require('autoprefixer');
 
 
 module.exports = {
 
   mode: 'development',
+
+  devServer: {
+    port: 9000,
+  },
 
   entry : {
     main : './src/index.js'
@@ -12,6 +19,54 @@ module.exports = {
   output : {
     filename : 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname,'src/index.html')
+    }),
+    new CleanWebpackPlugin(),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+              loader: 'style-loader'
+          },
+          {
+              loader: 'css-loader',
+              options: {
+                  sourceMap: true
+              }
+          },
+          {
+              loader: 'postcss-loader',
+              options: {
+                  sourceMap: true
+              }
+          },
+          {
+              loader: 'sass-loader',
+              options: {
+                  sourceMap: true
+              }
+          }
+        ],
+      },
+    ],
+  },
 
 }
